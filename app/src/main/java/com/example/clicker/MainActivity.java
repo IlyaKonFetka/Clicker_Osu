eefd4e8217e2fbdf77da8e49cf74ae15c869a346
Ilya <95120841+IlyaKonFetka@users.noreply.github.com>
2022-01-06 14:25:24 +0500

    huy2

--- app/src/main/java/com/example/clicker/MainActivity.java
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -6,8 +6,11 @@ import java.util.Formatter;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import android.annotation.SuppressLint;
+import android.view.View.OnTouchListener;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.Display;
 import android.view.View;
 import android.view.Window;
@@ -34,20 +37,23 @@ public class MainActivity extends AppCompatActivity {
         display.getSize(size);
         maxX = size.x;
         maxY = size.y;
-        //
+        /////////////////////////////////////////
 
         buttonHeight = butt1.getHeight();
         buttonWight = butt1.getWidth();
         butt2.setX((int)(Math.random() * (maxX - buttonWight)));
-        butt2.setY((int)(Math.random() * (maxY - buttonHeight-150)));
+        butt2.setY((int)(Math.random() * (maxY - buttonHeight)));
     }
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
-        SCORE = 0;
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        //SCORE = 0;
         scoreScreen = (TextView) findViewById(R.id.score);
         scoreScreen.setX(730);
         scoreScreen.setY(20);
@@ -58,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         butt1.setY(maxY/2);
         butt2 = findViewById(R.id.butt2);
         mainButton = findViewById(R.id.mainButton);
+
         buttonHeight = butt1.getHeight();
         buttonWight = butt1.getWidth();
 
@@ -69,39 +76,44 @@ public class MainActivity extends AppCompatActivity {
                 if (time == 2){
                     rand();
                     time = 0;
-                    //SCORE--;
-                    //scoreScreen.setText(Integer.toString(SCORE));
+                    SCORE -= 1;
+                    scoreScreen.setText(Integer.toString(SCORE));
                 }
             }
         }, 0, 300);
-        butt1.setOnClickListener(new View.OnClickListener() {
+
+        butt1.setOnTouchListener(new OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                butt1.setX(butt2.getX());
-                butt1.setY(butt2.getY());
-                rand();
-                count++;
-                time = 0;
-                SCORE ++;
-                scoreScreen.setText(Integer.toString(SCORE));
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    butt1.setX(butt2.getX());
+                    butt1.setY(butt2.getY());
+                    rand();
+                    count++;
+                    time = 0;
+                    SCORE += 2;
+                    scoreScreen.setText(Integer.toString(SCORE));
+                }
+                return false;
             }
         });
         butt2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SCORE -= 2;
+                SCORE -= 3;
                 scoreScreen.setText(Integer.toString(SCORE));
-                Toast.makeText(MainActivity.this, "LOSER", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "LOSER", Toast.LENGTH_SHORT).show();
             }
         });
         mainButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "LOSER", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "LOSER", Toast.LENGTH_SHORT).show();
                 SCORE -= 2;
                 scoreScreen.setText(Integer.toString(SCORE));
-
             }
         });
     }
 }
+//117
+//19.12.21-12:22
---
+++ app/src/main/java/com/example/clicker/WelcomeActivity.java
@@ -0,0 +1,28 @@
+package com.example.clicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class WelcomeActivity extends AppCompatActivity {
+ImageButton startB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        startB = findViewById(R.id.startButt);
+        Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
+        startB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+    }
+}
